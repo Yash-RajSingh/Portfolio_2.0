8931d6d76261d99327f0ecb34dda97e260356515
Yash-RajSingh <yashraj45202@gmail.com>
2025-09-11 10:44:02 +0530

    Removed unused imports

--- src/components/Loading/LoadingComponents.tsx
+++ src/components/Loading/LoadingComponents.tsx
@@ -1,4 +1,4 @@
-import styled, { keyframes, css } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Link } from 'react-router-dom';
 
 interface LoadingContainerProps {
